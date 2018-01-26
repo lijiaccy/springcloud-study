c07a64a431ff710f89caa675d2f25d699b0b6536
lijia <838865121@qq.com>
2018-01-26 14:47:00 +0800

    feign

---
+++ eurefa-Feign/client2/src/main/java/com/lijia/AppClient2.java
@@ -0,0 +1,27 @@
+package com.lijia;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class AppClient2 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppClient2.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi "+name+",i am from port:" +port;
+    }
+
+}
---
+++ eurefa-Feign/feign/src/main/java/com/lijia/service/HelloService.java
@@ -0,0 +1,12 @@
+package com.lijia.service;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "service-hi")
+public interface HelloService {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String callClientOne(@RequestParam(value = "name") String name);
+}
